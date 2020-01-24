bd28fcaf0a7acbaa4fcfb77387c81beb27edd979
German <galvaredo@prismamp.com>
2020-01-24 10:16:53 -0300

    Practica 2

---
+++ PracticaDML.sql
@@ -0,0 +1,21 @@
+-- Basándose en la tabla clientes realizar los siguientes puntos.
+
+-- 1- 	Insertar 5 clientes en la tabla clientes utilizando el insert into sin utilizar 
+-- campos como parte de la sentencias, es decir de la forma simplificada.
+-- 2-	Insertar 5 clientes en la tabla clientes utilizando los campos como parte de la 
+-- sentencias, es decir de la forma extendida. Completar solo los campos nombre, 
+-- apellido y CUIT.
+-- 3-	Actualizar el nombre del cliente 1 a Jose.
+-- 4-	Actualizar el nombre apellido y cuit del cliente 3 a Pablo Fuentes 20-21053119-0.
+-- 5-	Actualizar todos los comentarios NULL  a ‘ ‘.
+-- 6-	Eliminar los clientes con apellido Perez.
+-- 7-	Eliminar los clientes con CUIT Terminan en 0.
+
+
+
+
+-- Basando se en la tabla artículos, realizar los siguientes puntos.
+-- 	8- Aumentar un 20% los precios de los artículos con precio menor igual a 50.
+-- 	9- Aumentar un 15% los precios de los artículos con precio mayor a 50.
+-- 	10- Bajar un 5% los precios de los artículos con precio mayor a 200.
+-- 	11- Eliminar los artículos con stock menor a 10.
